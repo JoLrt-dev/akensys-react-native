32b92223bd1e5f21f5b60644162f9383d35c719e
JoLrt-dev <j.laurent1993@gmail.com>
2024-04-09 17:01:57 +0200

    create screens

---
+++ screens/Menu1Screen.js
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+export default function Menu1Screen() {
+  return (
+    <View>
+      <Text>Menu1Screen</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
---
+++ screens/Menu2Screen.js
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+export default function Menu2Screen() {
+  return (
+    <View>
+      <Text>Menu2Screen</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
